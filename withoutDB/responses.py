71d3ce52cdb7b9e1b4a111afcf7a7d2a82a1aaba
Yashraj Majahan <59019569+yashrajmahajan@users.noreply.github.com>
2021-08-08 19:30:36 +0530

    Update responses.py

--- withoutDB/responses.py
+++ withoutDB/responses.py
@@ -1,9 +1,1 @@
-from faunadb import query as q
-from faunadb.objects import Ref
-from faunadb.client import FaunaClient
-
-client = FaunaClient(secret="your-secret-here")
-
-indexes = client.query(q.paginate(q.indexes()))
-
-print(indexes)
+#suppoert file
