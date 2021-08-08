0429fdfcd6146603d55a5dd4fdb3156c01800e96
Yashraj Majahan <59019569+yashrajmahajan@users.noreply.github.com>
2021-08-08 19:21:12 +0530

    Add files via upload

---
+++ Echo_bot.py
@@ -0,0 +1,32 @@
+import logging
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO)
+
+logger = logging.getLogger(__name__)
+
+def book(update, context):
+    update.message.reply_text('Hi!')
+
+def echo(update, context):
+    update.message.reply_text(update.message.text)
+
+def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+
+    updater = Updater("1946266244:AAEifSpYvkVUQPzHFjK4wQQqdBm0gAvaEP8", use_context=True)
+    dp = updater.dispatcher
+
+    dp.add_handler(CommandHandler("book", book))
+
+    dp.add_handler(MessageHandler(Filters.text, echo))
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+    updater.idle()
+
+if __name__ == '__main__':
+    main()
